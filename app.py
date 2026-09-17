f5937da9dfbbdac815ae2515ebbb8ee5342e633c
Anne7106 <annecatherine.c2024@vitstudent.ac.in>
2026-09-17 10:26:42 +0530

    Jenkins pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ with open("student_report.txt", "w") as f:
     f.write("Student Name: Anca Catherine\n")
     f.write("Student ID: 24MIS0274\n")
     f.write("Total Marks: 270\n")
-    f.write("Average: 90.00\n")
+    f.write("Average: 90.0\n")
     f.write("Grade: S\n")
 
 print("Student report generated.")
