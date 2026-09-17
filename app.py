9d3370d9f401d32f50598686a81fd32ee1272c0d
Anne7106 <annecatherine.c2024@vitstudent.ac.in>
2026-09-17 09:29:02 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+with open("student_report.txt", "w") as f:
+    f.write("Student Management and Academic Performance Report\n")
+    f.write("Student Name: Anca Catherine\n")
+    f.write("Student ID: 24MIS0274\n")
+    f.write("Total Marks: 247\n")
+    f.write("Average: 82.33\n")
+    f.write("Grade: A\n")
+
+print("Student report generated.")
